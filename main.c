7b4d4114fcdfde4914589284975a383880b479d8
Shoxjaxon Xolmurodov <97885211+theshoxjaxon@users.noreply.github.com>
2023-07-06 22:44:32 +0500

    I have made switch and function ex all day

--- main.c
+++ main.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct Student {
+char name[20];
+char major[20];
+int age;
+double gpa;
+};
+int main() {
+    /*
 
-int main()
-{
-double num1;
-double num2;
-printf("Please enter first number: ");
-scanf("%lf", &num1);
-printf("Please enter second number: ");
-scanf("%lf", &num2);
-printf("Your answer is: %.2lf" , num1 + num2);
- return 0;
+char greade = 'a';
+
+
+switch(greade){
+case 'a':
+    printf("You did great job");
+    break;
+case 'b':
+    printf("You did allright");
+    break;
+case 'c':
+    printf("You should try again");
+    break;
+case 'd':
+    printf("You were close to fail");
+    break;
+case 'f':
+    printf("You have failed ");
+    break;
+default :
+    printf("Wrong grade !!");
+}
+*/
+struct Student student1;
+student1.age = 23;
+student1.gpa = 3.2;
+strcpy(student1.name , "Mike");
+
+printf("%.2f\n", student1.gpa);
+printf("%s", student1.name);s
+
+return 0;
 }
